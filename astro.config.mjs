1036c5314b71a8140eda888272c5e91c62aa9c93
andueza013 <tecnologicos12@gmail.com>
2026-07-24 17:30:48 +0200

    Add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Reemplaza con tu usuario y el nombre exacto de tu repositorio
+  site: 'https://andueza013.github.io',
+  base: '/PortfolioGameDev',
+});
