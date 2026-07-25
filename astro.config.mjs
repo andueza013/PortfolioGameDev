228bbd55bb1fe8cd596f70e531d76dbde8abca4a
andueza013 <tecnologicos12@gmail.com>
2026-07-25 11:21:39 +0200

    FIX: Broken links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Reemplaza con tu usuario y el nombre exacto de tu repositorio
   site: 'https://andueza013.github.io',
   base: '/PortfolioGameDev',
 });
